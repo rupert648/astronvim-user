c8520d87d00b98305c4336cbbc26addf24de9ff1
rupert648 <rupertcarr@hotmail.co.uk>
2024-03-13 10:34:10 +0000

    some stuff

--- init.lua
+++ init.lua
@@ -51,6 +51,50 @@ return {
     -- enable servers that you already have installed without mason
     servers = {
       -- "pyright"
+      "eslint",
+      "prettier"
+    },
+  },
+
+  plugins = {
+    {
+      "jay-babu/mason-null-ls.nvim",
+      opts = {
+        handlers = {
+          -- for prettier
+          prettier = function()
+            require("null-ls").register(require("null-ls").builtins.formatting.prettier.with {
+              condition = function(utils)
+                return utils.root_has_file "package.json"
+                  or utils.root_has_file ".prettierrc"
+                  or utils.root_has_file ".prettierrc.json"
+                  or utils.root_has_file ".prettierrc.js"
+              end,
+            })
+          end,
+          -- for prettierd
+          prettierd = function()
+            require("null-ls").register(require("null-ls").builtins.formatting.prettierd.with {
+              condition = function(utils)
+                return utils.root_has_file "package.json"
+                  or utils.root_has_file ".prettierrc"
+                  or utils.root_has_file ".prettierrc.json"
+                  or utils.root_has_file ".prettierrc.js"
+              end,
+            })
+          end,
+          -- For eslint_d:
+          eslint_d = function()
+            require("null-ls").register(require("null-ls").builtins.diagnostics.eslint_d.with {
+              condition = function(utils)
+                return utils.root_has_file "package.json"
+                  or utils.root_has_file ".eslintrc.json"
+                  or utils.root_has_file ".eslintrc.js"
+              end,
+            })
+          end,
+        },
+      },
     },
   },
 
@@ -63,7 +107,23 @@ return {
         disabled_plugins = { "tohtml", "gzip", "matchit", "zipPlugin", "netrwPlugin", "tarPlugin" },
       },
     },
-  },
+  },  
+  ["null-ls"] = function()
+    local status_ok, null_ls = pcall(require, "null-ls")
+    if status_ok then
+      null_ls.setup {
+        debug = false,
+        sources = {
+          null_ls.builtins.formatting.prettier,
+        },
+        on_attach = function(client)
+          if client.resolved_capabilities.document_formatting then
+            vim.cmd "autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()"
+          end
+        end,
+      }
+    end
+  end,
 
   -- This function is run last and is a good place to configuring
   -- augroups/autocommands and custom filetypes also this just pure lua so
